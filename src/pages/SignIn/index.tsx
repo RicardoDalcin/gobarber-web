c86b384771fe52e291597c2dc46d80a26dc92722
Ricardo <ricardo.dalcinh@gmail.com>
2020-09-05 15:40:51 -0300

    Setup linting and create SignIn page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="E-mail" />
+
+          <input placeholder="Senha" type="password" />
+
+          <button type="submit">Entrar</button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </form>
+
+        <a href="signup">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
