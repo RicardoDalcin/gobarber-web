d1eb855211d04356128cb7b33861172e517ca4e8
Ricardo <ricardo.dalcinh@gmail.com>
2020-09-06 10:34:51 -0300

    Create SignOut method and change context to hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import GlobalStyles from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -11,7 +11,7 @@ import { Container, Content, Background } from './styles';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 
 interface SignInFormData {
   email: string;
